db3d83743173bff4a97c94df2e593e43a3e9bb74
Daniel Plisetsky <danplisetsky@gmail.com>
2019-07-19 12:57:25 +0300

    refactor: wrap main function in try-catch

--- cli.js
+++ cli.js
@@ -27,8 +27,17 @@ function processArgs(argv) {
 
   let finalArgs;
   if (parsedArgs.config) {
-    let config = JSON.parse(fs.readFileSync(parsedArgs.config).toString());
-    finalArgs = Object.assign({}, config, parsedArgs);
+    try {
+      let config = JSON.parse(fs.readFileSync(parsedArgs.config).toString());
+      finalArgs = Object.assign({}, config, parsedArgs);
+    } catch (error) {
+      throw new Error(
+        "Error parsing the config file: " +
+          chalk.yellow(parsedArgs.config) +
+          "\n" +
+          error.message
+      );
+    }
   } else {
     finalArgs = parsedArgs;
   }
@@ -70,88 +79,89 @@ function mergeChapters(
   // Read all the folders in the provided directory
   // whose names end with digit(s)
 
-  try {
-    const chapterDirs = getChapterDirs(directory);
+  const chapterDirs = getChapterDirs(directory);
 
-    if (chapterDirs && chapterDirs.length === 0) {
-      throw new Error("no chapter directories found");
-    }
+  if (chapterDirs && chapterDirs.length === 0) {
+    throw new Error("no chapter directories found");
+  }
 
-    // Get a consecutive list of all .md files we'll need to merge
-    /** @type{Array<string>} */
-    const chapterFiles = chapterDirs.flatMap(dir => {
-      const files = fs.readdirSync(dir);
-      return files.filter(f => /md$/.test(f)).map(f => path.join(dir, f));
-    });
+  // Get a consecutive list of all .md files we'll need to merge
+  /** @type{Array<string>} */
+  const chapterFiles = chapterDirs.flatMap(dir => {
+    const files = fs.readdirSync(dir);
+    return files.filter(f => /md$/.test(f)).map(f => path.join(dir, f));
+  });
 
-    // Map the .md files to their content and add headers
-    const chapterContents = chapterFiles.map(ch => {
-      const header = addHeaders ? "## " + ch.match(/(\d+)\.md$/)[1] + "\n" : "";
-      return header + fs.readFileSync(ch).toString();
-    });
+  // Map the .md files to their content and add headers
+  const chapterContents = chapterFiles.map(ch => {
+    const header = addHeaders ? "## " + ch.match(/(\d+)\.md$/)[1] + "\n" : "";
+    return header + fs.readFileSync(ch).toString();
+  });
 
-    // Read the subtitle file, if exists
-    const subtitleContent = subtitle ? readSubtitleFile(subtitle) : "";
+  // Read the subtitle file, if exists
+  const subtitleContent = subtitle ? readSubtitleFile(subtitle) : "";
 
-    const finalVersionMarkdown =
-      "# " +
-      title +
-      "\n" +
-      subtitleContent +
-      "\n\n" +
-      chapterContents.join("\n");
-    if (!fs.existsSync(finalFolder)) {
-      fs.mkdirSync(finalFolder, { recursive: true });
-    }
-    const finalPath = path.join(finalFolder, title + ".md");
-    fs.writeFileSync(finalPath, finalVersionMarkdown);
+  const finalVersionMarkdown =
+    "# " + title + "\n" + subtitleContent + "\n\n" + chapterContents.join("\n");
+  if (!fs.existsSync(finalFolder)) {
+    fs.mkdirSync(finalFolder, { recursive: true });
+  }
+  const finalPath = path.join(finalFolder, title + ".md");
+  fs.writeFileSync(finalPath, finalVersionMarkdown);
 
-    console.log(chalk.green("success: ") + `wrote ${finalPath}`);
+  console.log(chalk.green("success: ") + `wrote ${finalPath}`);
 
-    if (pandocFormat) {
-      /** @type {string} */ (pandocFormat).split(",").forEach(pf => {
-        const tempPandocFile = "__temp" + pf;
-        const panodcArgs = `-f markdown -t ${pf} -o ` + tempPandocFile;
+  if (pandocFormat) {
+    /** @type {string} */ (pandocFormat).split(",").forEach(pf => {
+      const tempPandocFile = "__temp" + pf;
+      const panodcArgs = `-f markdown -t ${pf} -o ` + tempPandocFile;
 
-        pandoc(finalVersionMarkdown, panodcArgs, (error, _result) => {
-          if (error) return;
+      pandoc(finalVersionMarkdown, panodcArgs, (error, _result) => {
+        if (error) return;
 
-          const finalPath = path.join(finalFolder, title + "." + pf);
-          fs.renameSync(tempPandocFile, finalPath);
-          console.log(chalk.green("success: ") + `wrote ${finalPath}`);
-        });
+        const finalPath = path.join(finalFolder, title + "." + pf);
+        fs.renameSync(tempPandocFile, finalPath);
+        console.log(chalk.green("success: ") + `wrote ${finalPath}`);
       });
-    }
-  } catch (error) {
-    console.log(chalk.red("failure: "), error.message);
-    process.exit(1);
+    });
   }
 }
 
 function main() {
-  const {
-    directory = process.cwd(),
-    title = path.basename(directory),
-    finalFolder = directory,
-    pandocFormat = null,
-    addHeaders = false,
-    watch = false,
-    watchInterval = 1000,
-    subtitle = null
-  } = processArgs(process.argv.slice(2));
+  try {
+    const {
+      directory = process.cwd(),
+      title = path.basename(directory),
+      finalFolder = directory,
+      pandocFormat = null,
+      addHeaders = false,
+      watch = false,
+      watchInterval = 1000,
+      subtitle = null
+    } = processArgs(process.argv.slice(2));
 
-  if (watch) {
-    const watcher = chokidar.watch(directory, {
-      // ignore .dotfiles and the final folder output
-      ignored: [/(^|[\/\\])\../, `${path.join(finalFolder, title)}*`]
-    });
-    const debouncedMergeChapters = debounce(
-      mergeChapters,
-      watchInterval,
-      false
-    );
-    watcher.on("all", (_event, _path) => {
-      debouncedMergeChapters(
+    if (watch) {
+      const watcher = chokidar.watch(directory, {
+        // ignore .dotfiles and the final folder output
+        ignored: [/(^|[\/\\])\../, `${path.join(finalFolder, title)}*`]
+      });
+      const debouncedMergeChapters = debounce(
+        mergeChapters,
+        watchInterval,
+        false
+      );
+      watcher.on("all", (_event, _path) => {
+        debouncedMergeChapters(
+          title,
+          directory,
+          finalFolder,
+          pandocFormat,
+          addHeaders,
+          subtitle
+        );
+      });
+    } else {
+      mergeChapters(
         title,
         directory,
         finalFolder,
@@ -159,16 +169,10 @@ function main() {
         addHeaders,
         subtitle
       );
-    });
-  } else {
-    mergeChapters(
-      title,
-      directory,
-      finalFolder,
-      pandocFormat,
-      addHeaders,
-      subtitle
-    );
+    }
+  } catch (error) {
+    console.log(chalk.red("failure: "), error.message);
+    process.exit(1);
   }
 }
 
